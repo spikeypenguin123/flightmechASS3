a933c2b38eee6be71e7897161426dcb2edea05a9
Adam-Hammo <adamhammo99@gmail.com>
2021-10-15 22:07:04 +1100

    Renamed some vars

--- sim/Integrate.m
+++ sim/Integrate.m
@@ -4,18 +4,18 @@ function x_next = Integrate(state, t_step)
     % NOTE: This is just pseudocode until StateRates is implemented.
     
     dx_1 = StateRates(state);
-    A_n = dx_1.*t_step;
+    A = dx_1.*t_step;
     
-    dx_2 = StateRates(state+A_n./2);
-    B_n = dx_2.*t_step;
+    dx_2 = StateRates(state+A./2);
+    B = dx_2.*t_step;
     
-    dx_3 = StateRates(state+B_n./2);
-    C_n = dx_3.*t_step;
+    dx_3 = StateRates(state+B./2);
+    C = dx_3.*t_step;
     
-    dx_4 = StateRates(state+C_n);
-    D_n = dx_4.*t_step;
+    dx_4 = StateRates(state+C);
+    D = dx_4.*t_step;
 
-    x_next = state + 1/6.*(A_n+2*B_n+2*C_n+D_n);
+    x_next = state + 1/6.*(A+2*B+2*C+D);
     
 end
 
