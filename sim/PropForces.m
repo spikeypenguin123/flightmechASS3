a73183489674e0280ed980235f7e7449ea82c179
maxschill99 <45658938+maxschill99@users.noreply.github.com>
2021-10-15 23:51:32 +1100

    Edited functions

--- sim/PropForces.m
+++ sim/PropForces.m
@@ -1,9 +1,12 @@
 function  [Pmax, Thrust] = PropForces(sigma, eta, V_T, delta_T, Pmax_SL)
-    
-    % Computing Propulsion Fores
-    
-    Pmax = Pmax_SL*(1.1324*sigma - 0.1324); % Max Power
-    Thrust = ((eta*Pmax)/V_T)*delta_T; % Thrust
+  
+% INPUT:
+% OUTPUT:
+
+%% Computing Propulsion Fores
+
+Pmax = Pmax_SL*(1.1324*sigma - 0.1324); % Max Power
+Thrust = ((eta*Pmax)/V_T)*delta_T; % Thrust
 
 
 end
