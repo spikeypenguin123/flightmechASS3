583d566e397b1f763e561031d3ef253f4acb685b
Maxim Schiller <maximschiller99@gmail.com>
2021-10-17 19:44:20 +1100

    Update FM_Task1A.m

--- Task A/FM_Task1A.m
+++ Task A/FM_Task1A.m
@@ -7,7 +7,7 @@ close all
 [ FlightData ] = aero3560_LoadFlightDataPC9_nominalCG1();
 % [ FlightData ] = aero3560_LoadFlightDataPC9_CG2();
 
-Inert = FlightData.Inertial;
+Inertial = FlightData.Inertial;
 CLim = FlightData.ControlLimits;
 Aero = FlightData.Aero;
 Geom = FlightData.Geo;
@@ -17,15 +17,15 @@ Geom = FlightData.Geo;
 % alpha = -10:0.5:10;
 % Cl = (alpha - Aero.alpha_o)*Aero.CLa;
 xac = 0.25;
-g = Inert.g;
-m = Inert.m;
+g = Inertial.g;
+m = Inertial.m;
 W = m*g;
 V = 100/1.944; % Speed at 100 knots in m/s
 rho_US = 0.9711*0.002377; % Density at 1000ft
 rho = rho_US*515;
 Q = 0.5*rho*(V^2)*Geom.S;
 
-%% TASK 1
+%% TASK 1 - Part (a)
 
 % Initialising headings
 psi_a = deg2rad(-25);
@@ -33,14 +33,20 @@ gamma = deg2rad(-7);
 theta_a = gamma;
 beta = deg2rad(-7);
 
-% Computing stability equation in order to find equlibrium CL
+% Computing stability equation in order to find equlibrium CL, alpha,
+% elevator position and throtte position
 
 % Setting convergence parameters
 kmax = 500;
 eps = 10^-3;
 
 % Initial guesses
-Cl0 = 0.5;
+Cl = 0.5;
+% Cl0 = 
+% Clalpha = 
+alpha = (Cl - Cl0)/Clalpha;
+delta_T0 = 0.5;
+delta_e0 = 0;
 
 Cl = W/Q;
 for k = 1:kmax
@@ -51,3 +57,10 @@ for k = 1:kmax
         
     end
 end
+
+
+%% Task 1 - Part (b)
+
+
+
+%% Task 1 - Part (c)
